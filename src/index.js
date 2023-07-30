c665bf383d4874bfa3bed020696cc83b05fcc16e
Matias Turra <matias.turra@symmetry-systems.com>
2023-07-30 12:13:56 -0300

    organize file structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+export * from './components/AutocompleteTextField'
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>ro
+// );
